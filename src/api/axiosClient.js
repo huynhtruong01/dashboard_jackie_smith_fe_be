ca4fd8a9bb16d10b26ff85950e3bf2b18a50f268
Huynh Truong <huynhphuoctruongk17086@gmail.com>
2022-10-30 23:10:22 +0700

    update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const axiosClient = axios.create({
     baseURL: 'https://backend-jackie-smith.onrender.com/api',
+    // baseURL: 'http://localhost:5000/api',
     headers: {
         'Content-Type': 'application/json',
     },
